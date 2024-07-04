496f21a17f2b39bc6307a93364be8be9b2882c49
TrevorNight <159196928+TrevorNight@users.noreply.github.com>
2024-07-03 21:38:39 -0300

    Add files via upload

--- UTN-PROGRAMACION/Programacion_I/05_misc/01_menu_arrays_funciones/Package_Arrays/Array_Generales.py
+++ UTN-PROGRAMACION/Programacion_I/05_misc/01_menu_arrays_funciones/Package_Arrays/Array_Generales.py
@@ -1,14 +1,72 @@
-print("Ingrese 10 numeros enteros entre -1000 y 1000")
-numero = input("ingrese un numero")
+from Especificas import *
+from os import system
 
-menu = ("a-Mostrar la cantidad de números positivos y negativos.\
-       \nb-Mostrar la sumatoria de los números pares.\
-       \nc-Informar el mayor de los números impares.\
-       \nd-Listar todos los números ingresados.\
-       \ne-Listar todos los números pares.\
-       \nf-Listar los números de las posiciones impares.\
-       \ng-Salir")
-print(menu)
+def mostrar_menu():
+    mensaje = "Ingrese un numero entero:"
+    mensaje_error = "Error, el numero debe estar entre -1000 y 1000"
+    minimo = -1000
+    maximo = 1000
+    cantidad_numeros = 10
 
-menu_opcion = input("que hacer")
+    lista_numeros = pedir_numeros_enteros(mensaje, mensaje_error, minimo, maximo, cantidad_numeros)
 
+    while True:
+        
+        menu =  ("1_Mostrar la cantidad de números positivos y negativos.\
+                \n2_Mostrar la sumatoria de los números pares.\
+                \n3_Informar el mayor de los números impares.\
+                \n4_Listar todos los números ingresados.\
+                \n5_Listar todos los números pares.\
+                \n6_Listar los números de las posiciones impares.\
+                \n7_Salir.\
+                \nElija una opcion:")
+
+        opcion = input(menu)
+
+        match opcion:
+            case "1":
+                numeros_positivos = 0
+                numeros_negativos = 0
+                    
+                for posicion in range(cantidad_numeros):
+                    numero = determinar_positivo_negativo(lista_numeros[posicion])
+
+                    if numero:
+                        numeros_positivos = numeros_positivos + 1
+                    else:
+                        numeros_negativos = numeros_negativos + 1
+                
+                print (f"Numeros positivos: {numeros_positivos} \
+                    \nNumeros negativos: {numeros_negativos}")
+                
+            case "2":
+                suma_pares = sumar_numeros_pares(lista_numeros, cantidad_numeros)
+                print(suma_pares)
+
+            case "3":
+                numero_impar_mayor = mostrar_numero_impar_mayor(lista_numeros, cantidad_numeros)
+                print(numero_impar_mayor)
+
+            case "4":
+                mostrar_lista(lista_numeros)
+            case "5":
+                cantidad_pares = contar_pares(lista_numeros, cantidad_numeros)
+                lista_pares = [0] * cantidad_pares
+                posicion2 = -1
+                for posicion in range(cantidad_numeros):
+                    if lista_numeros[posicion] % 2 == 0:
+                        posicion2 = posicion2 + 1
+                        lista_pares[posicion2] = lista_numeros[posicion]
+                mostrar_lista(lista_pares)
+            
+            case "6":
+                lista_posicion_impares = listar_posicion_numeros_impares(lista_numeros, cantidad_numeros)
+                mostrar_lista(lista_posicion_impares)
+
+            case "7":
+                break
+
+        system("pause")    
+        system("cls")
+
+mostrar_menu()
--- UTN-PROGRAMACION/Programacion_I/05_misc/01_menu_arrays_funciones/Package_Arrays/Especificas.py
+++ UTN-PROGRAMACION/Programacion_I/05_misc/01_menu_arrays_funciones/Package_Arrays/Especificas.py
@@ -1,7 +1,66 @@
-def determinar_positivo_negativo(numero:str) -> True|False:
-    if numero[0] in "-":
+from Package_Input.Input import *
+    
+def pedir_numeros_enteros(mensaje:str, mensaje_error:str, minimo:int, maximo:int, cantidad_numeros:int) -> list:
+    lista_numeros = [0] * cantidad_numeros
+    for posicion in range(cantidad_numeros):
+        print(posicion + 1, "° numero ")
+        numero = None
+        while numero == None:
+            numero = get_int(mensaje, mensaje_error, minimo, maximo, 0)                      
+        lista_numeros[posicion] = numero
+
+    return lista_numeros
+
+def determinar_positivo_negativo(numero) -> True|False :
+    if numero < 0:
         return False
     else:
         return True
+
+def sumar_numeros_pares(lista_numeros:list, cantidad_numeros:int) -> int:
+    suma_pares = 0
+    for posicion in range(cantidad_numeros):
+        if lista_numeros[posicion] % 2 == 0:
+            suma_pares = suma_pares + lista_numeros[posicion]
     
+    return suma_pares
+
+def mostrar_numero_impar_mayor(lista_numeros:list, cantidad_numeros:int) -> int:
+    primer_numero = True
+    for posicion in range(cantidad_numeros):
+        if lista_numeros[posicion] % 2 != 0:
+            if primer_numero or numero_impar_mayor < lista_numeros[posicion]:
+                numero_impar_mayor = lista_numeros[posicion]
+                primer_numero = False
     
+    return numero_impar_mayor
+
+def contar_pares(lista_numeros:list, cantidad_numeros:int) -> int:
+    cantidad_pares = 0
+    for posicion in range(cantidad_numeros):
+        if lista_numeros[posicion] % 2 == 0:
+            cantidad_pares = cantidad_pares + 1
+    
+    return cantidad_pares
+
+def mostrar_lista (lista:list):
+    len_lista = len(lista)
+    for posicion in range(len_lista):
+        print(lista[posicion])
+
+def listar_posicion_numeros_impares(lista_numeros:list , cantidad_numeros:int) ->list:
+    cantidad_impares = 0
+    for posicion in range(cantidad_numeros):
+        if lista_numeros[posicion] % 2 != 0:
+            cantidad_impares = cantidad_impares + 1
+    
+    lista_posicion_impares = [0] * cantidad_impares
+    posicion2 = -1
+    for posicion in range(cantidad_numeros):
+        if lista_numeros[posicion] % 2 != 0:
+            posicion2 = posicion2 + 1
+            lista_posicion_impares[posicion2] = posicion
+
+    return lista_posicion_impares
+    
+
